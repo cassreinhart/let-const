47935418a81c48afefaff59de95ba3bdec5f54b1
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2022-08-16 16:58:51 -0500

    adding let-const exercises

---
+++ let-const.js
@@ -0,0 +1,5 @@
+const PI = 3.14;
+//var has a global scope and the declaration runs before everything else, while const has block scope and is not hoisted. Let can be reassigned, but var cannot.
+//var has global scope and the declaration runs before everything else, while const has block scope and is not hoisted
+//let can be reassigned but const cannot
+//hoisting is using variables before they are declared and not getting an error. Essentially the variables declarations run before the rest of the code.
